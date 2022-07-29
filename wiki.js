dfc0695e792c6be4c973428a55e051bd0b8e9783
Kurtis <kurtis@edge.app>
2022-07-28 18:27:29 -0700

    stylings

--- wiki.js
+++ wiki.js
@@ -1,7 +1,11 @@
 const searchButton = document.querySelector('#search')
+const inputWord = document.getElementById('#input-word')
+let searchUrl =
+  'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch='
 
 searchButton.addEventListener('click', (behavior) => {
   //  Prevent the default behaviour
   behavior.preventDefault()
-  console.log('button pressed')
+
+  console.log(inputWord)
 })
